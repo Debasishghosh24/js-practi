f9a0f141d3be753c1b0564007e18eeecb9d13086
Debasish ghosh <ghoshsuman1435@gmail.com>
2026-03-27 18:53:52 +0530

    refactor indexShuffle function to separate even and odd character handling

--- edabit/indexshuffle.js
+++ edabit/indexshuffle.js
@@ -1,11 +1,19 @@
 
 function indexShuffle(str){
     let store = "";
+
+    let odd = ""
+    let even = ""
     for(let i=0; i<str.length; i++) {
         if(i % 2 === 0) {
-            store = store + (str[i]);
+            even = even + (str[i]);
+        }else{
+            odd = odd + (str[i]);
         }
+        
     }
+    store = (even + odd);
+    
     return store;
 }
 console.log(indexShuffle("abcdefg"));
